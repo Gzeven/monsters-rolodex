328c14c0c5c3ef44cfedb2164b03a64c6c6dd641
Gerben van de Weerdhof <gerben1978@hotmail.com>
2020-02-20 19:22:36 +0100

    Updates

---
+++ precache-manifest.0ec1f96fa91440a8d6386d446aca785e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab849be25ea0fb85deebfde23ab7c805",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b8c37c76920e11c88bb",
+    "url": "/monsters-rolodex/static/css/main.117ab350.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8c37c76920e11c88bb",
+    "url": "/monsters-rolodex/static/js/main.898880f9.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
